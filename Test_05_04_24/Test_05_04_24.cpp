2b6e9cdfe2590697758aca09ec292fb5772d0774
KonstantinovIKM723a <alex7wrt@gmail.com>
2024-04-04 20:11:22 +0300

    Add project files.

---
+++ Test_05_04_24/Test_05_04_24.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>;
+#include "windows.h";
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    //
+    int a = 1;
+
+    int* pa = &a;
+    int* pb = &a;
+
+    int m, n, k = 0;
+
+    int* x = new int[n];
+    int* y = new int[m];
+
+    srand(time(NULL));
+
+    cout << "Введіть значення змінної a: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) x[i] = rand() % 19 - 9;
+}
